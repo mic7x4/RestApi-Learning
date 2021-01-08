460451d9b9e8daa2a08581664c620973b6dc6b74
mic7x4 <mic7x4@gmail.com>
2021-01-08 10:36:32 +0200

    [features] connecting to mysql using node

--- App.js
+++ App.js
@@ -1,28 +1,46 @@
 // Load our server using express somehow...
 const express = require('express');
 const morgan = require('morgan');
-
+const mysql = require('mysql');
 
 
 const app = express();
 app.use(morgan('short'))
 const PORT = 3000;
-
+const connection = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'rest__api'
+})
 
 
 app.get('/',(req,res) => {
     console.log('Responding to root Route');
     res.send('<h1> Hello from Root</h1>')
 });
+
 // get users
 app.get('/users',(req,res) => {
-    let users = [{id:1, firstname:'Michael',lastName:"Crook"},
-                {id:2, firstname:'Netphantom',lastName:"Crook"},
-                {id:3, firstname:'Kanigra',lastName:"James"},
-            ];
-    res.json(users);
+    connection.query("SELECT * FROM users",(err,rows,fields) =>{
+        res.json(rows);
+    })
+    
 });
 
+app.get('/users/:id',(req,res)=>{
+    const id = req.params.id;
+    const userQuery = `SELECT * FROM users WHERE id= ?`;
+    if(userQuery){
+        connection.query(userQuery,[id],(err,rows,fields) =>{
+        res.json(rows);
+    })  
+    }else{
+        res.send('user with given id can not be found');
+    }
+});
+
+// Listening to the port 
 app.listen(PORT,() => {
     console.log(`Server is up and Running on ${PORT}`);
 })
